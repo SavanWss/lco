37092eaad3ab40f19c66e332f85997490a3f3d02
SavanWss <savanwss@gmail.com>
2023-02-23 10:38:38 +0530

    new login page

--- lib/screens/general/signIn/SignInScreen.dart
+++ lib/screens/general/signIn/SignInScreen.dart
@@ -45,186 +45,260 @@ class _SignInScreenState extends State<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/logo.png"), opacity: 0.7)),
-      child: Form(
-        key: _FormKey,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          child: ListView(
-            children: [
-              // GestureDetector(
-              //   onHorizontalDragStart: (details) {
-              //     Navigator.popUntil(
-
-              //       , (route) => false)
-              //   },
-              // )
-
-              // email text field
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: TextFormField(
-                  autofocus: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    suffixIcon: Icon(Icons.email),
-                    labelStyle: TextStyle(fontSize: 15),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    errorStyle: TextStyle(
-                        color: Color.fromARGB(255, 255, 0, 0), fontSize: 10),
-                  ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/login.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: const EdgeInsets.only(left: 35, top: 80),
+            child: const Text(
+              "Welcome\nBack",
+              style: TextStyle(color: Colors.white, fontSize: 33),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  right: 35,
+                  left: 35,
+                  top: MediaQuery.of(context).size.height * 0.5),
+              child: Column(children: [
+                TextFormField(
                   controller: emailController,
-                  validator: ((value) {
-                    if (value!.isEmpty || value == null) {
-                      return "Enter the Email";
-                    } else if (!value.contains("@")) {
-                      return "Enter the Valid Email";
-                    } else {
-                      return null;
-                    }
-                  }),
-                ),
-              ),
-
-              // password text field
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: TextFormField(
-                  autofocus: false,
-                  obscureText: true,
-                  keyboardType: TextInputType.visiblePassword,
                   decoration: InputDecoration(
-                    labelText: "password",
-                    suffixIcon: Icon(Icons.calendar_view_day_outlined),
-                    labelStyle: TextStyle(fontSize: 15),
+                    fillColor: Colors.grey.shade100,
+                    filled: true,
+                    hintText: 'Email',
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    errorStyle: TextStyle(
-                        color: Color.fromARGB(255, 255, 0, 0), fontSize: 10),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  controller: passwordController,
-                  validator: ((value) {
-                    // if (value!.isEmpty || value == null) {
-                    //   return "Enter the password";
-                    // } else {
-                    //   return null;
-                    // }
-                    return null;
-                  }),
                 ),
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey.shade100,
+                    filled: true,
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Color(0xff4c505b),
+                        fontSize: 27,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: const Color(0xff4c505b),
+                      child: IconButton(
+                        color: Colors.white,
+                        onPressed: () async {
+                          print("button is pressed");
 
-              // SignIn Button
-              ElevatedButton(
-                  onPressed: () async {
-                    print("button is pressed");
-                    if (_FormKey.currentState!.validate()) {
-                      CircularIndicator.startCircularIndicator(context);
+                          CircularIndicator.startCircularIndicator(context);
 
-                      bool interNetConnectionFlag =
-                          await InterNetConnectivityChecker
-                              .interNetConnectivityChecker();
-                      print(interNetConnectionFlag);
-                      if (interNetConnectionFlag == false) {
-                        Toast.toastView(msg: "connect to network!!!");
-                        Navigator.of(context).pop();
-                        return;
-                      }
+                          bool interNetConnectionFlag =
+                              await InterNetConnectivityChecker
+                                  .interNetConnectivityChecker();
+                          print(interNetConnectionFlag);
+                          if (interNetConnectionFlag == false) {
+                            Toast.toastView(msg: "connect to network!!!");
+                            Navigator.of(context).pop();
+                            return;
+                          }
+print("email ===== ${emailController.text}");
+print("password ===== ${passwordController.text}");
+                          // signin method is called
+                          var login = await FbSignIn.fbSignIn(
+                              email: emailController.text,
+                              password: passwordController.text);
 
-                      // signin method is called
-                      var login = await FbSignIn.fbSignIn(
-                          email: emailController.text,
-                          password: passwordController.text);
+                          //  login validation
+                          print("login === $login");
+                          if (login["status"] == true) {
+                            var userData = login["data"][0];
+                            if (userData["profession"] == null ||
+                                userData["married_status"] == null ||
+                                userData["yearly_income"] == null) {
+                              userData["profession"] =
+                                  userData["married_status"] = "";
+                              userData["yearly_income"] = 0;
+                            }
 
-                      //  login validation
-                      print("login === $login");
-                      if (login["status"] == true) {
-                        var userData = login["data"][0];
-                        if (userData["profession"] == null || userData["married_status"] == null || userData["yearly_income"] == null) {
-                        userData["profession"] = userData["married_status"] = "";
-                        userData["yearly_income"] = 0;
+                            LogInDataSaver.logInDataSave(
+                                name: userData["name"],
+                                mobile: userData["mobile"],
+                                email: userData["email"],
+                                dob: userData["dob"],
+                                role: userData["role"],
+                                gender: userData["gender"],
+                                address: userData["address"],
+                                profession: userData["profession"],
+                                totaldonatedfund: userData["total_dnt_fund"],
+                                adoptedchild: userData["adopted_child"],
+                                yearlyincome: 0,
+                                marriedstatus: userData["married_status"]);
 
-                        }
+                            if (LoggedInDetails.userRole == "user") {
+                              Navigator.pushReplacementNamed(
+                                  context, ScreenRoutes.userDashboardScreen);
+                            } else {
+                              Navigator.pushReplacementNamed(
+                                  context, ScreenRoutes.adminDashboardScreen);
+                            }
+                          } else {
+                            // Toast View
+                            Toast.toastView(msg: "login Failed");
+                            Navigator.of(context).pop();
+                          }
+                        },
+                        icon: const Icon(Icons.arrow_forward),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          print("button is pressed signUp");
+                          Navigator.pushNamed(
+                              context, ScreenRoutes.signUpScreen);
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Color(0xff4c505b),
+                          ),
+                        ),
+                      ),
+             
+                    ]),
+              ]),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+//   ElevatedButton(
+              //     onPressed: () async {
+              //       print("button is pressed");
+              //       if (_FormKey.currentState!.validate()) {
+              //         CircularIndicator.startCircularIndicator(context);
+
+              //         bool interNetConnectionFlag =
+              //             await InterNetConnectivityChecker
+              //                 .interNetConnectivityChecker();
+              //         print(interNetConnectionFlag);
+              //         if (interNetConnectionFlag == false) {
+              //           Toast.toastView(msg: "connect to network!!!");
+              //           Navigator.of(context).pop();
+              //           return;
+              //         }
+
+              //         // signin method is called
+              //         var login = await FbSignIn.fbSignIn(
+              //             email: emailController.text,
+              //             password: passwordController.text);
+
+              //         //  login validation
+              //         print("login === $login");
+              //         if (login["status"] == true) {
+              //           var userData = login["data"][0];
+              //           if (userData["profession"] == null || userData["married_status"] == null || userData["yearly_income"] == null) {
+              //           userData["profession"] = userData["married_status"] = "";
+              //           userData["yearly_income"] = 0;
+
+              //           }
 
                    
 
-                        LogInDataSaver.logInDataSave(
-                            name: userData["name"],
-                            mobile: userData["mobile"],
-                            email: userData["email"],
-                            dob: userData["dob"],
-                            role: userData["role"],
-                            gender: userData["gender"],
-                            address: userData["address"],
-                            profession: userData["profession"],
-                            totaldonatedfund: userData["total_dnt_fund"],
-                            adoptedchild: userData["adopted_child"],
-                            yearlyincome: 0,
-                            marriedstatus: userData["married_status"]);
+              //           LogInDataSaver.logInDataSave(
+              //               name: userData["name"],
+              //               mobile: userData["mobile"],
+              //               email: userData["email"],
+              //               dob: userData["dob"],
+              //               role: userData["role"],
+              //               gender: userData["gender"],
+              //               address: userData["address"],
+              //               profession: userData["profession"],
+              //               totaldonatedfund: userData["total_dnt_fund"],
+              //               adoptedchild: userData["adopted_child"],
+              //               yearlyincome: 0,
+              //               marriedstatus: userData["married_status"]);
 
-                        if (LoggedInDetails.userRole == "user") {
-                          Navigator.pushReplacementNamed(
-                              context, ScreenRoutes.userDashboardScreen);
-                        } else {
-                          Navigator.pushReplacementNamed(
-                              context, ScreenRoutes.adminDashboardScreen);
-                        }
-                      } else {
-                        // Toast View
-                        Toast.toastView(msg: "login Failed");
-                        Navigator.of(context).pop();
-                      }
-                    }
-                  },
-                  child: Text("SignIn")),
+              //           if (LoggedInDetails.userRole == "user") {
+              //             Navigator.pushReplacementNamed(
+              //                 context, ScreenRoutes.userDashboardScreen);
+              //           } else {
+              //             Navigator.pushReplacementNamed(
+              //                 context, ScreenRoutes.adminDashboardScreen);
+              //           }
+              //         } else {
+              //           // Toast View
+              //           Toast.toastView(msg: "login Failed");
+              //           Navigator.of(context).pop();
+              //         }
+              //       }
+              //     },
+              //     child: Text("SignIn")),
 
-              Center(
-                child: Text(
-                  "or",
-                  style: TextStyle(
-                      color: Color.fromRGBO(82, 21, 72, 1), fontSize: 15),
-                ),
-              ),
-
-              // SignUp Button
-              ElevatedButton(
-                  onPressed: () async {
-                    print("button is pressed signUp");
-                    Navigator.pushNamed(context, ScreenRoutes.signUpScreen);
-
-                    var afterResponse = await Notifications.sendNotification(
-                        deviceToken:
-                            "dOrRN-smSPeObaHZ2TdDcL:APA91bH1H7aKKxxMViBLnrDXRifXDnwltqFrA1Hztt1XkOVUtDN8iM39OyWCNhOwU80S5u1CZZX3pGo39hV7xJYPOSWAbzcHWU4zVhvcAkHcNKgINvePKuWeku86tfm3NM1KkVDG4hVO",
-                        title: "first from application",
-                        body: "body");
-
-                    print(afterResponse);
-                  },
-                  child: Text("SignUp")),
-
-              //           ProgressButton(
-              // borderRadius: BorderRadius.all(Radius.circular(8)),
-              // strokeWidth: 2,
-              // child: Text(
-              //   "Sample",
-              //   style: TextStyle(
-              //     color: Colors.white,
-              //     fontSize: 24,
+              // Center(
+              //   child: Text(
+              //     "or",
+              //     style: TextStyle(
+              //         color: Color.fromRGBO(82, 21, 72, 1), fontSize: 15),
               //   ),
               // ),
-              // onPressed: (AnimationController controller) async {
-              //   await httpJob(controller);
-              // }
-            ],
-          ),
-        ),
-      ),
-    ));
-  }
-}
+
+              // // SignUp Button
+              // ElevatedButton(
+              //     onPressed: () async {
+              //       print("button is pressed signUp");
+              //       Navigator.pushNamed(context, ScreenRoutes.signUpScreen);
+
+              //       var afterResponse = await Notifications.sendNotification(
+              //           deviceToken:
+              //               "dOrRN-smSPeObaHZ2TdDcL:APA91bH1H7aKKxxMViBLnrDXRifXDnwltqFrA1Hztt1XkOVUtDN8iM39OyWCNhOwU80S5u1CZZX3pGo39hV7xJYPOSWAbzcHWU4zVhvcAkHcNKgINvePKuWeku86tfm3NM1KkVDG4hVO",
+              //           title: "first from application",
+              //           body: "body");
+
+              //       print(afterResponse);
+              //     },
+              //     child: Text("SignUp")),
